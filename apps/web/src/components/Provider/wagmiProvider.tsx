3991d07c90a83d3d15c47877328d28aefa7fa51d
ericli <helloeric01@gmail.com>
2023-08-17 16:55:06 +0800

    fix: updated typescript to 5.0.4

--- apps/web/src/components/Provider/wagmiProvider.tsx
+++ apps/web/src/components/Provider/wagmiProvider.tsx
@@ -2,8 +2,7 @@
 
 import { Chain, configureChains, createConfig, WagmiConfig } from 'wagmi'
 import { goerli, polygonMumbai, sepolia } from 'wagmi/chains'
-import { ConnectKitProvider, ConnectKitButton, getDefaultConfig } from "connectkit"
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { ConnectKitProvider, getDefaultConfig } from "connectkit"
 import { publicProvider } from 'wagmi/providers/public'
 import * as React from 'react';
 
@@ -17,27 +16,17 @@ const { chains, publicClient, webSocketPublicClient } = configureChains(
     defaultChains,
     [publicProvider()],
 )
-// const config = createConfig({
-//     autoConnect: true,
-//     connectors: [
-//         new MetaMaskConnector({ chains }),
-//     ],
-//     publicClient,
-//     webSocketPublicClient,
-// });
 
 const config = createConfig(
     getDefaultConfig({
         autoConnect: true,
-        publicClient,
-        webSocketPublicClient,
+        walletConnectProjectId: 'walletConnectProjectId',
         // Required API Keys
-        alchemyId: process.env.ALCHEMY_ID, // or infuraId
-        walletConnectProjectId: process.env.WALLETCONNECT_PROJECT_ID,
-        chains: defaultChains,
-
+        chains: chains,
         // Required
         appName: "Tobi",
+        publicClient,
+        webSocketPublicClient,
     }),
 );
 
