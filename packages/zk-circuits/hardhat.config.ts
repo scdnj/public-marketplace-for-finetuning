15be7dff73996d04f3e301a1bce55fc74dc253f0
sasakura1994 <kanazawa01211123@gmail.com>
2023-08-22 10:35:48 +0800

    feat: zkml circuits

--- packages/zk-circuits/hardhat.config.ts
+++ packages/zk-circuits/hardhat.config.ts
@@ -115,10 +115,10 @@ const config: HardhatUserConfig = {
   },
   circom: {
     inputBasePath: './circuits',
-    ptau: 'https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_15.ptau',
+    ptau: 'https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_20.ptau',
     circuits: [
       {
-        name: 'himitsu',
+        name: 'circuits',
       },
     ],
   },
