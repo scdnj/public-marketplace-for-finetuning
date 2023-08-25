3d7121a7a80ce6524099eabaf6bbf11de76b5aec
Brian <pianochicken1996@gmail.com>
2023-08-25 11:09:34 +0800

    feat: add reset & remove access

--- packages/tezos-interact/src/index.ts
+++ packages/tezos-interact/src/index.ts
@@ -1,5 +1,5 @@
 import { TezosToolkit } from '@taquito/taquito';
-import { InMemorySigner, importKey } from '@taquito/signer';
+import { InMemorySigner } from '@taquito/signer';
 
 const Tezos = new TezosToolkit('https://ghostnet.tezos.marigold.dev');
 
@@ -7,30 +7,23 @@ Tezos.setProvider({
   signer: new InMemorySigner('edskS1zsQPHwdhuQQ9dcvtmrgSC1f7pqMZMrst4xzB4hMgkmAzgnDHbRhHGckG7RXN6yoed1PE31S63z2Y7Pa13kvZcQgcQ8bU'),
 });
 
-
-export const accessProposal = async () =>  {
-  await Tezos.contract
-  .at('KT1KqDtnTZMYiEpe81f6u24Jnmq5Hg2h9z5v')
-  .then((contract) => {
-    console.log('Access proposal...')
-    return contract.methods.access().send();
-  })
-  .then((op) => {
-    const result: any = op.results[0]
-    console.log(`Waiting for ${op.hash} to be confirmed...`);
-    return op.confirmation(1).then(() => op.hash);
-  })
-  .then((hash) => {
-    console.log(`Access operation finished: https://better-call.dev/ghostnet/opg/${hash}`)
-  })
-  .catch((error) => {
-    console.log(`Access error: ${JSON.stringify(error, null, 2)}`)
-  })
+interface ProposalType {
+  [index: number]: string;
 }
 
-export const voteProposal = async (vote: number) =>  {
+const proposal: ProposalType = { 
+  1: 'KT1ExMeV39XPUEyARJYwjUMfC1Lq1PLwo8bh', 
+  2: 'KT1KnNfPjgnn7jznstBZ7FXvwBEH5RwjLafK',
+  3: 'KT1S9iHKShcrkJLZMTeeEcqgKZ3gQQgmcKBg'
+}
+
+export const voteProposal = async (proposalNumber: number, vote: number) =>  {
+  if(proposalNumber > 3 || proposalNumber < 1) {
+    return Error
+  }
+  
   await Tezos.contract
-  .at('KT1KqDtnTZMYiEpe81f6u24Jnmq5Hg2h9z5v')
+  .at(proposal[proposalNumber])
   .then((contract) => {
     console.log('Vote proposal...')
     return contract.methods.vote(vote).send();
@@ -47,13 +40,26 @@ export const voteProposal = async (vote: number) =>  {
   });
 } 
 
-export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+export const resetProposal = async (proposalNumber: number) =>  {
+  if(proposalNumber > 3 || proposalNumber < 1) {
+    return Error
+  }
 
-const main = async () => {
-  // await voteProposal(1)
-  // sleep(15000)
-  // await accessProposal()
+  await Tezos.contract
+  .at(proposal[proposalNumber])
+  .then((contract) => {
+    console.log('Reset proposal...')
+    return contract.methods.reset().send();
+  })
+  .then((op) => {
+    console.log(`Waiting for ${op.hash} to be confirmed...`);
+    return op.confirmation(1).then(() => op.hash);
+  })
+  .then((hash) => {
+    console.log(`Reset operation finished: https://better-call.dev/ghostnet/opg/${hash}`)
+    return hash
+  })
+  .catch((error) => {
+    console.log(`Reset error: ${JSON.stringify(error, null, 2)}`)
+  })
 }
-
-main()
-
